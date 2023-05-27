d1f8416e915b2172496405077ae05f5a54c37203
Vincent Knecht <vincent.knecht@mailoo.org>
2023-05-27 09:10:15 +0200

    Adapt fstab for qcom-msm8916 devices
    Signed-off-by: Vincent Knecht <vincent.knecht@mailoo.org>

--- platform/fstab/fstab.cpp
+++ platform/fstab/fstab.cpp
@@ -11,6 +11,8 @@
 #define __FILE_ENCRYPT__ fileencryption=aes-256-xts:aes-256-cts
 #endif
 
+#ifndef platform_qcom_msm8916
+
 system                              /system         ext4    ro,barrier=1                          wait,first_stage_mount,logical,slotselect,avb=vbmeta_system,avb_keys=/avb
 system_ext                          /system_ext     ext4    ro,barrier=1                          wait,first_stage_mount,logical,slotselect,avb=vbmeta_system
 product                             /product        ext4    ro,barrier=1                          wait,first_stage_mount,logical,slotselect,avb=vbmeta_system
@@ -30,4 +32,16 @@ vendor_dlkm                         /vendor_dlkm    ext4    ro,noatime,errors=pa
 /devices/platform/scb/<ALL>.pcie/<ALL>/<ALL>/<ALL>/usb<ALL>    auto   auto    defaults            voldmanaged=usb:auto,encryptable=userdata
 #else
 /devices/platform/soc/<ALL>/usb<ALL>                  auto            auto    defaults            voldmanaged=usb:auto,encryptable=userdata
+#endif
+#else
+/dev/block/platform/soc@0/7824900.mmc/by-name/system    /system            ext4    noatime,ro,errors=panic wait,first_stage_mount
+/dev/block/platform/soc@0/7824900.mmc/by-name/cache     /vendor            ext4    noatime,ro,errors=panic wait,first_stage_mount
+/dev/block/platform/soc@0/7824900.mmc/by-name/userdata  /data              ext4    noatime,nosuid,nodev,barrier=1 wait,check,latemount,quota,formattable,__FILE_ENCRYPT__
+
+// For firmwares
+// /dev/block/platform/soc@0/7824900.mmc/by-name/modem    /vendor/odm/modem    vfat    ro,shortname=lower,uid=1000,gid=1000,dmask=227,fmask=337,context=u:object_r:firmware_file:s0 wait
+/dev/block/platform/soc@0/7824900.mmc/by-name/modem     /vendor/odm/modem    vfat    defaults wait
+/dev/block/platform/soc@0/7824900.mmc/by-name/persist   /vendor/odm/persist  ext4    noatime,ro,noauto_da_alloc wait
+
+/devices/platform/soc@0/7864900.mmc/mmc_host/mmc*       auto                 auto    defaults     voldmanaged=sdcard1:auto
 #endif
