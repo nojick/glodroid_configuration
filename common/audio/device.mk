f6111d64ba9d6317cebbecf4819d27cc84ef9fcb
nojick <nojick@gmail.com>
2024-04-27 20:31:52 +0300

    Revert "add android.hardware.audio.sounddose-vendor-impl"
    This reverts commit 23a2fa6588b2ac77021f973ee8103ae8bc3e8a5d.

--- common/audio/device.mk
+++ common/audio/device.mk
@@ -8,7 +8,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.service \
     android.hardware.audio@7.1-impl \
     android.hardware.audio.effect@7.0-impl \
-    android.hardware.audio.sounddose-vendor-impl \
     audio.primary.$(TARGET_PRODUCT) \
     audio.usb.default \
     audio.r_submix.default \
