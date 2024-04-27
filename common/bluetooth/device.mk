d6e338977fbecb29cb5b3d565e09aea3b2e8971b
nojick <nojick@gmail.com>
2024-04-27 20:31:41 +0300

    Update BT an14
    bluetooth  work

--- common/bluetooth/device.mk
+++ common/bluetooth/device.mk
@@ -4,9 +4,11 @@ PRODUCT_PACKAGES += android.hardware.bluetooth@1.1-service.btlinux
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml \
-    frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration_7_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration_7_0.xml \
+
+PRODUCT_VENDOR_PROPERTIES += \
+    bluetooth.core.le.vendor_capabilities.enabled=false
 
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth.audio@2.1-impl \
     android.hardware.bluetooth.audio-impl \
     audio.bluetooth.default \
